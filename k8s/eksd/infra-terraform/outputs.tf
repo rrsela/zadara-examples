dfbe4f25d4ba4293e61e38afebf620f78d98685e
Liron <liron.ostrovsky@gmail.com>
2023-09-14 11:56:29 +0300

    Backward comp

--- k8s/eksd/infra-terraform/outputs.tf
+++ k8s/eksd/infra-terraform/outputs.tf
@@ -7,19 +7,19 @@ output "environment" {
 }
 
 output "vpc_id" {
-  value = aws_vpc.eksd_vpc.id
+  value = tostring(aws_vpc.eksd_vpc.id)
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.eksd_public.id
+  value = tostring(aws_subnet.eksd_public.id)
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.eksd_private.id
+  value = tostring(aws_subnet.eksd_private.id)
 }
 
 output "security_group_id" {
-  value = aws_security_group.eksd_k8s.id
+  value = tostring(aws_security_group.eksd_k8s.id)
 }
 
 output "masters_load_balancer_id" {
@@ -31,10 +31,10 @@ output "masters_load_balancer_internal_dns" {
 }
 
 output "bastion_ip" {
-  value = aws_eip.bastion.public_ip
+  value = tostring(aws_eip.bastion.public_ip)
 }
 
 output "x_loadbalancer_script" {
   description = "Pointer to script which can get the Load Balancer private & public IPs"
-  value       = "${path.module}/get_loadbalancer.sh ${var.api_endpoint} ${aws_eip.bastion.public_ip} ${local.nlb_private_dns} <access_key> <secret_key> <bastion_user> <bastion_key>"
+  value       = tostring("${path.module}/get_loadbalancer.sh ${var.api_endpoint} ${aws_eip.bastion.public_ip} ${local.nlb_private_dns} <access_key> <secret_key> <bastion_user> <bastion_key>")
 }
