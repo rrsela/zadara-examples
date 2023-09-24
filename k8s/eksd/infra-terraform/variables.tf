411143827ddbac3f2d7fab73d0ea8b959cec92d1
Liron <liron.ostrovsky@gmail.com>
2023-09-24 14:08:04 +0300

    Added cluster root ca

--- k8s/eksd/infra-terraform/variables.tf
+++ k8s/eksd/infra-terraform/variables.tf
@@ -68,6 +68,5 @@ variable "bastion_instance_type" {
 
 variable "root_ca_cert_path" {
   type        = string
-  default     = ""
   description = "Path to the root certificate authority certificate of the cluster"
 }
