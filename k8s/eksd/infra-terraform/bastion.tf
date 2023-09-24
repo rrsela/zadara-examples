c641092bc344948ca7f8e74b1b51522f1e22fcc7
Liron <liron.ostrovsky@gmail.com>
2023-09-24 13:37:16 +0300

    Added cluster root ca

--- k8s/eksd/infra-terraform/bastion.tf
+++ k8s/eksd/infra-terraform/bastion.tf
@@ -8,7 +8,25 @@ data "cloudinit_config" "root-ca-trust-config" {
       ca-certs:
         trusted:
           - |
-            ${file(var.root_ca_cert_path)}
+            -----BEGIN CERTIFICATE-----
+            MIIDBDCCAeygAwIBAgIUIfgV/b7LgG4Irp0lgQoj5pDiXXAwDQYJKoZIhvcNAQEL
+            BQAwGzEZMBcGA1UEAwwQbW9ua2V5LWF1dGhvcml0eTAeFw0yMzA5MjQwOTEyMjha
+            Fw0yNDA5MjMwOTEyMjhaMBsxGTAXBgNVBAMMEG1vbmtleS1hdXRob3JpdHkwggEi
+            MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDhc5nngkp6ZAHiq7kOX2bDQb3Z
+            dbqytKeKY9vdvNn/RQuPrF4ARycDQeKQq3aroT9CTGaiWRGm1Ujah2UgbvDb1VkL
+            V8+lGTxonkVrinoA9SruYoVrc/eI0egY9fYV18zRjFD2bT8EZecm/wLBbXcEXKfe
+            X2ABSBP7aT+ShByxjTEA94XjTkbTXmoNIc2yPaSxeHeqf127Al0aODoFd3Kkh0VN
+            zYnkuVUJip8HfpkSt3CqSM9c2UyA0QIM2DV21ITdjquE0oU1Lf7iLX+5yVSmwXxQ
+            o2KyHrCFNQZz4fNPhT1zdR4XFTlkucKBrnyHEVhZKozMxqH7qxsCspARaM77AgMB
+            AAGjQDA+MA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgKkMBsGA1UdEQQU
+            MBKCEG1vbmtleS1hdXRob3JpdHkwDQYJKoZIhvcNAQELBQADggEBAFI6XkOk5rN9
+            byVt35vBDL2WgLL9yfn0KtIAuuGpto+TfH2bwoYlcB9DxEaebQveaBHTcmQfzme4
+            zyX13Pj+jLOzvnAZm6mjyWqtWdWkEA4wrjBjZCIznTmw6ehHB8qG+RuZLdiuY4jz
+            74uXwAurqgEjwVUVkSFLBfceEcCFL9lrXbcFBdq0eFjpbkXEFbk7Jyu8b4UnExDN
+            8pIKdX9h9zRxHpIzoMzfuPImdr/Er1iBg//XXvSErbXIWqG0fJwSr7rckvcbOQ2J
+            vqz3GAqsYzw8BWes++ymSpJEaNwAi1JfLKdpJgmrpK7TbRtRGCORzKMhtBh12laP
+            YS21qZJjhbc=
+            -----END CERTIFICATE-----
     EOF
   }
 }
