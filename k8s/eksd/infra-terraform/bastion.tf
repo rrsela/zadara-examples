a9c8e76dab395dc9ac08133b36958ff01c94a8d5
Liron <liron.ostrovsky@gmail.com>
2023-09-24 13:08:55 +0300

    Added cluster root ca

--- k8s/eksd/infra-terraform/bastion.tf
+++ k8s/eksd/infra-terraform/bastion.tf
@@ -1,3 +1,18 @@
+data "cloudinit_config" "root-ca-trust-config" {
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+      #cloud-config
+      ca-certs:
+        trusted:
+          - |
+            ${file(var.root_ca_cert_path)}
+    EOF
+  }
+}
+
 resource "aws_eip" "bastion" {
   instance = aws_instance.bastion.id
   vpc      = true
@@ -11,12 +26,14 @@ resource "aws_instance" "bastion" {
   key_name               = var.bastion_keyname
   subnet_id              = aws_subnet.eksd_public.id
   vpc_security_group_ids = [aws_security_group.eksd_k8s.id]
-  user_data              = <<-EOF
-                    #!/bin/bash
-                    set -e
-                    cat ${file(var.root_ca_cert_path)} > /usr/local/share/ca-certificates/root_ca.crt
-                    update-ca-certificates
-                  EOF
+  user_data              = data.cloudinit_config.server_config.rendered
+
+  # user_data              = <<-EOF
+  #                   #!/bin/bash
+  #                   set -e
+  #                   cat ${file(var.root_ca_cert_path)} > /usr/local/share/ca-certificates/root_ca.crt
+  #                   update-ca-certificates
+  #                 EOF
   tags = {
     Name = "bastion"
   }
